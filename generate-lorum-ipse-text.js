670012e485b3b2f12586797d53a75d4e4bd94b48
sarkiroka <sarki_roka@freemail.hu>
2020-08-15 00:36:08 +0200

    lórum ipse chrome extension

---
+++ generate-lorum-ipse-text.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * @author sarkiroka on 2020.08.14.
+ */
+const DEBUG = false;
+
+function debug() {
+	if (DEBUG) {
+		console.log.apply(null, arguments);
+	}
+}
+
+let lorumIpseNextText = '';
+
+function sendText(id, noMoreTry) {
+	debug('lorum ipse elküldésre kerül', {last: noMoreTry});
+	chrome.tabs.sendMessage(id, {action: 'fill-with-lorum-ipse', text: lorumIpseNextText}, function (p) {
+		if (chrome.runtime.lastError) {
+			debug('opszli', chrome.runtime.lastError.message);
+			if (!noMoreTry) {
+				sendText(id, true);
+			}
+		}
+	});
+}
+
+function getNextText(callback) {
+	const apiUrl = 'http://www.lorumipse.hu/generate/';
+	const KIND_OF_WORD = 2;
+	const THE_WORD = 0;
+	const WORD_ALIGN = 3;
+	const ALIGN_LEFT = 'left';
+	const EMPTY_STRING = '';
+	const SPACE = ' ';
+	const PUNCTIATION = 'PUNCT';
+
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState === XMLHttpRequest.DONE) {
+			let response = JSON.parse(xhr.responseText);
+			const createSentence = words => words.reduce((sentence, wordDefinition) => {
+				let notNeedSpace = !sentence || (wordDefinition[KIND_OF_WORD] == PUNCTIATION && wordDefinition[WORD_ALIGN] == ALIGN_LEFT);
+				sentence += (notNeedSpace ? EMPTY_STRING : SPACE) + wordDefinition[THE_WORD];
+				return sentence;
+			}, EMPTY_STRING);
+			let text = response.map(createSentence).join(' ');
+			lorumIpseNextText = text;
+			debug('lórum ipse elmondta');
+			if (callback) {
+				callback();
+			}
+		}
+	};
+	xhr.open('GET', apiUrl, true);
+	xhr.send();
+}
+
+getNextText();
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+	if (lorumIpseNextText) {
+		sendText(tab.id);
+		getNextText();
+	} else {
+		getNextText(() => {
+			sendText(tab.id);
+		})
+	}
+});
+
+chrome.contextMenus.create({
+	title: 'Lórum Ipse mondja',
+	contexts: ['editable'],
+	onclick: (info, tab) => {
+		sendText(tab.id);
+	}
+});
---
+++ lorum-ipse-content-script.js
@@ -0,0 +1,89 @@
+/**
+ * Lórum Ipse szöveg érkezte esetén az aktuális input mező aktuális poziciójába betölti a szöveget
+ * legalábbis megpróbálja ezt
+ * @author sarkiroka on 2020.08.14.
+ */
+const DEBUG = false;
+const MAX_ITERATION_COUNT = 50;
+
+function debug() {
+	if (DEBUG) {
+		console.log.apply(null, arguments);
+	}
+}
+
+debug('lorum ipse figyel');
+
+function getActiveElement(document, iterationCount = 0) {
+	if (iterationCount > MAX_ITERATION_COUNT) {
+		return false;
+	}
+	document = document || window.document;
+	if (document.body === document.activeElement || document.activeElement.tagName == 'IFRAME') {
+		// Get iframes
+		var iframes = document.getElementsByTagName('iframe');
+		for (let i = 0; i < iframes.length; i++) {
+			let focused = getActiveElement(iframes[i].contentWindow.document, iterationCount + 1);
+			if (focused !== false) {
+				return focused; // The focused
+			}
+		}
+	} else {
+		let focused = document.activeElement;
+		while (focused.shadowRoot && focused.shadowRoot.activeElement && iterationCount < MAX_ITERATION_COUNT) {
+			focused = focused.shadowRoot.activeElement;
+			iterationCount++;
+		}
+		return focused;
+	}
+
+	return false;
+}
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if (message.action == 'fill-with-lorum-ipse') {
+		debug('lorum ipse íróját megszólították', {message, element});
+		let element = null;
+		try {
+			element = getActiveElement(document);
+		} catch (e) {
+			console.warn('nem lehet elérni az aktív elemet', e);
+			sendResponse(false);
+		}
+		if (element) {
+			const VALID_TYPES = ['TEXT', 'SEARCH'];
+			const isTextInputField = element.tagName.toUpperCase() == 'INPUT' && (element.getAttribute('type') == null || VALID_TYPES.includes(element.getAttribute('type').toUpperCase()));
+			const isTextarea = element.tagName == 'TEXTAREA';
+			const oldValue = element.value;
+			let newValue = '';
+			let position = 0;
+			element.focus();
+			if (isTextInputField) {
+				const start = element.selectionStart;
+				const end = element.selectionEnd;
+				newValue = oldValue.substr(0, start) + message.text + oldValue.substr(end);
+				position = start + message.text.length;
+				element.value = newValue;
+				element.setSelectionRange(position, position);
+			} else if (isTextarea) {
+				const selection = document.getSelection();
+				const start = Math.min(selection.anchorOffset, selection.focusOffset);
+				const end = Math.max(selection.anchorOffset, selection.focusOffset);
+
+				newValue = oldValue.substr(0, start) + message.text + oldValue.substr(end);
+				position = start + message.text.length;
+				const range = document.createRange();
+				range.setStart(element, position);
+				range.setEnd(element, position);
+				range.collapse(true);
+				selection.removeAllRanges();
+				selection.addRange(range);
+			} else {
+				console.warn('there is no active input element', element);
+			}
+			sendResponse(true);
+		}
+	} else {
+		sendResponse(false);
+	}
+});
